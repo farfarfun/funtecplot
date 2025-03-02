e494f3dd1253090a211fcfb2c68e816186a96f20
farfun <1007530194@qq.com>
2025-03-02 16:25:47 +0800

    add

--- src/funtecplot/dump/triangle.py
+++ src/funtecplot/dump/triangle.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from .base import Base
+from funtecplot.dump.base import Base
+
+# https://blog.csdn.net/weixin_43095105/article/details/125128913
 
 
 class TriangleData(Base):
@@ -27,8 +29,26 @@ class TriangleData(Base):
         else:
             return f"{data:6f}"
 
+    def format_int(self, data):
+        if isinstance(data, list) or isinstance(data, np.ndarray):
+            return " ".join([f"{int(data[k])}" for k in range(len(data))])
+        else:
+            return f"{data:6f}"
+
     def _dump(self):
         for i in range(self.point.shape[0]):
             yield f"{self.data_format(self.point[i])} {self.data_format(self.data[i])}"
         for i in range(self.edge.shape[0]):
-            yield f"{self.data_format(self.edge[i])}"
+            yield f"{self.format_int(self.edge[i])}"
+
+
+def example():
+    TriangleData(
+        variables=["x", "y", "ux", "uy"],
+        point=np.random.random((10, 2)),
+        data=np.random.random((10, 2)),
+        edge=np.array([[0, 1, 3], [1, 2, 4], [2, 0, 9]]),
+    ).dump("001.csv")
+
+
+# example()
